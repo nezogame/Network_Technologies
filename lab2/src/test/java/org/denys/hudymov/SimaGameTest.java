6c0dd861ac650f73a315392f434d5465ca87487e
Denys <denys.hudymov@gmail.com>
2023-11-02 00:48:23 +0200

    Created tests for adding lines and checking whether someone wins or not. AppTest.java was deleted

---
+++ lab2/src/test/java/org/denys/hudymov/SimaGameTest.java
@@ -0,0 +1,62 @@
+package org.denys.hudymov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimaGameTest {
+    SimaGame simaGame = new SimaGame();
+    @Test
+    public void testAllCorner(){
+        simaGame.initializeMiddle();
+        simaGame.run(3, 2);
+        simaGame.run(3, 1);
+        simaGame.run(3, 0);
+        simaGame.run(2, 0);
+        simaGame.run(1, 0);
+        simaGame.run(0, 0);
+        simaGame.run(0, 1);
+        simaGame.run(0, 2);
+        simaGame.run(0, 3);
+        simaGame.run(0, 4);
+        simaGame.run(0, 5);
+        simaGame.run(0, 6);
+        simaGame.run(0, 6);
+        simaGame.run(1, 6);
+        simaGame.run(2, 6);
+        simaGame.run(3, 6);
+        simaGame.run(4, 6);
+        simaGame.run(5, 6);
+        simaGame.run(6, 6);
+        simaGame.run(6, 5);
+        simaGame.run(6, 4);
+        simaGame.run(6, 3);
+        simaGame.run(6, 2);
+        simaGame.run(6, 1);
+        simaGame.run(6, 0);
+        simaGame.run(5, 0);
+        simaGame.run(4, 0);
+        System.out.println(simaGame.printBoard());
+    }
+
+    @Test
+    public void testWinCorner(){
+        simaGame.initializeMiddle();
+        simaGame.run(3, 2);
+        simaGame.run(3, 1);
+        simaGame.run(3, 0);
+        simaGame.run(2, 0);
+        simaGame.run(1, 0);
+        simaGame.run(0, 0);
+        simaGame.run(0, 1);
+        simaGame.run(0, 2);
+        simaGame.run(0, 3);
+        simaGame.run(1, 3);
+        simaGame.run(2, 3);
+        simaGame.run(2, 2);
+        simaGame.run(1, 1);
+        var s = simaGame.run(1, 2);
+        System.out.println(s);
+        System.out.println(simaGame.printBoard());
+    }
+}
